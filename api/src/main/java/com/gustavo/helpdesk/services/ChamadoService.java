ddad9235faf0e91c303429e98883e842bcb8884d
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-25 08:32:47 -0300

    findById chamado

---
+++ api/src/main/java/com/gustavo/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.gustavo.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavo.helpdesk.domain.Chamado;
+import com.gustavo.helpdesk.repositories.ChamadoRespository;
+import com.gustavo.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+    
+    @Autowired
+    private ChamadoRespository chamadoRespository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = chamadoRespository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+}
